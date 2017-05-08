b7c8be11acfc946d40084972b74b82106bbc04cb
James Hunt <james@niftylogic.com>
2017-05-08 14:27:20 -0400

    Fix virtualbox / kvm mismatch
    VirtualBox 5.1 now does KVM virtualization.  Neat.  So now, we have to
    look for some BMC facts as well as the `virtual` fact.

--- scripts/virtualbox.sh
+++ scripts/virtualbox.sh
@@ -2,13 +2,14 @@
 set -e
 
 # Bail if we are not running inside VirtualBox.
-if [[ `facter virtual` != "virtualbox" ]]; then
+if [[ `facter virtual` != "virtualbox" && `facter boardproductname` != "VirtualBox" ]]; then
+    echo "not running under virtualbox (`facter virtual` / `facter boardproductname`)"
     exit 0
 fi
 
 mkdir -p /mnt/virtualbox
 mount -o loop /home/vagrant/VBoxGuest*.iso /mnt/virtualbox
-/mnt/virtualbox/VBoxLinuxAdditions.run
+/mnt/virtualbox/VBoxLinuxAdditions.run || true
 ln -s /opt/VBoxGuestAdditions-*/lib/VBoxGuestAdditions /usr/lib/VBoxGuestAdditions
 umount /mnt/virtualbox
 rm -rf /home/vagrant/VBoxGuest*.iso
